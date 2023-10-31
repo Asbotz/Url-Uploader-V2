f8b4e616e86cab64a404c6e91b6af8272a9090b2
Asbotz <144653726+Asbotz@users.noreply.github.com>
2023-10-31 17:42:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ echo "Cloning Repo...."
 git clone https://github.com/EliteCraftStudios/UPLOADER-BOT /UPLOADER-BOT
 cd /UPLOADER-BOT
 pip3 install -U -r requirements.txt
-echo "Starting Bot.... Please Wait. Check You Log. Bot Edit by @god_luffy_ati"
+echo "Starting Bot.... Please Wait. Check You Log"
 python3 bot.py
